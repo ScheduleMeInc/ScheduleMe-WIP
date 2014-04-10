2c7cbd2fbc99c919ffb5ff87fa552363d8f246d4
Vinh Tang <radsine@gmail.com>
2014-04-09 23:58:54 -0500

    Change out password.

--- connect.php
+++ connect.php
@@ -4,7 +4,7 @@ include("inc/caldav-client.php");
 
 echo "connect to caldav server: ";
 
-$cal = new CalDAVClient("https://p26-caldav.icloud.com/schedulemeinc@gmail.com/calendars", "schedulemeinc@gmail.com", "Testitn0w");
+$cal = new CalDAVClient("https://p26-caldav.icloud.com/schedulemeinc@gmail.com/calendars", "schedulemeinc@gmail.com", "password");
 $options = $cal->DoOptionsRequest();
 print_r($options);
 
@@ -15,20 +15,20 @@ foreach ( $events AS $k => $event ) {
 }
 
 
-/*
+
 $acc = array();
 $acc["google"] = array(
 "user"=>"schedulemeinc@gmail.com",
-"pass"=>"Testitn0w",
+"pass"=>"password",
 "server"=>"ssl://www.google.com",
 "port"=>"443",
-"uri"=>"https://apidata.googleusercontent.com/caldav/v2/schedulemeinc@gmail.com/events/"
+"uri"=>"https://apidata.googleusercontent.com/caldav/v2/schedulemeinc@gmail.com/events/",
 );
 
-$account = $acc["davical"];
+$account = $acc["google"];
 $cal = new CalDAVClient( $account["uri"], $account["user"], $account["pass"], "", $account["server"], $account["port"] );
 $options = $cal->DoOptionsRequest();
 print_r($options);
-*/
+
 
 ?>

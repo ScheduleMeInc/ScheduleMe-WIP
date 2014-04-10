c1973c61e3046c295e25a47fc106f0a4c68f6c61
Vinh Tang <radsine@gmail.com>
2014-04-09 23:30:53 -0500

    Using davical and successfully authenticated to Apple iCloud calendar.

---
+++ connect.php
@@ -0,0 +1,34 @@
+<?php
+
+include("inc/caldav-client.php");
+
+echo "connect to caldav server: ";
+
+$cal = new CalDAVClient("https://p26-caldav.icloud.com/schedulemeinc@gmail.com/calendars", "schedulemeinc@gmail.com", "Testitn0w");
+$options = $cal->DoOptionsRequest();
+print_r($options);
+
+// Fetch all events for February
+$events = $cal->GetEvents("20140401T000000Z","20140430T000000Z");
+foreach ( $events AS $k => $event ) {
+  echo( $event['data'] );
+}
+
+
+/*
+$acc = array();
+$acc["google"] = array(
+"user"=>"schedulemeinc@gmail.com",
+"pass"=>"Testitn0w",
+"server"=>"ssl://www.google.com",
+"port"=>"443",
+"uri"=>"https://apidata.googleusercontent.com/caldav/v2/schedulemeinc@gmail.com/events/"
+);
+
+$account = $acc["davical"];
+$cal = new CalDAVClient( $account["uri"], $account["user"], $account["pass"], "", $account["server"], $account["port"] );
+$options = $cal->DoOptionsRequest();
+print_r($options);
+*/
+
+?>
